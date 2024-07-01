b349a2a97df8ba6d7ff0f590e20bc6633d3c4ed8
dionmar-lariosa <main.dionmar@gmail.com>
2024-07-01 12:07:54 +0800

    view all users

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  async getAll(
+    @Query('cursor', ParseIntPipe) cursor: number = 0,
+    @Query('pageSize', ParseIntPipe) pageSize: number = 10,
+  ) {
+    try {
+      return await this.userService.getAll(cursor, pageSize);
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+}
