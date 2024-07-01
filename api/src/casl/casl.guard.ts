819eab0cf78acc958d41ecc428a5625e294a35c9
dionmar-lariosa <main.dionmar@gmail.com>
2024-07-01 14:06:25 +0800

    ignore casl auth for public

--- api/src/casl/casl.guard.ts
+++ api/src/casl/casl.guard.ts
@@ -7,7 +7,7 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
-import { CHECK_ABILITY, RequiredRule } from 'src/app.decorator';
+import { CHECK_ABILITY, IS_PUBLIC_KEY, RequiredRule } from 'src/app.decorator';
 import { TokenPayload_i } from 'src/auth/auth.interface';
 import { CaslAbilityFactory } from 'src/casl/casl-ability.factory/casl-ability.factory';
 
@@ -19,6 +19,15 @@ export class CaslGuard implements CanActivate {
   ) {}
 
   canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      // 💡 return true for routes that has public decorator
+      return true;
+    }
+
     const rules =
       this.reflector.get<RequiredRule[]>(CHECK_ABILITY, context.getHandler()) ||
       [];
